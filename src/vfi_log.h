f981a1a1a4560a95106f7a6be108e64df080c500
Thomas Staale Hauge <tsh@vmetro.no>
2008-05-19 11:17:15 -0700

    Fixed VFI_DEBUG macro. Changed some names to differentiate log and debug.

--- src/vfi_log.h
+++ src/vfi_log.h
@@ -4,9 +4,6 @@
 
 #include <syslog.h>
 #include <stdarg.h>
-#include <assert.h>
-
-/* Debug level is treated as a four-bit level integer, when, and a 28-bit mask, who_what */
 
 /* The when level follows syslog conventions: */
 #define	VFI_LOG_EMERG	        LOG_EMERG	/* system is unusable			*/
@@ -17,50 +14,50 @@
 #define	VFI_LOG_NOTICE          LOG_NOTICE	/* normal but significant condition	*/
 #define	VFI_LOG_INFO	        LOG_INFO	/* informational			*/
 #define	VFI_LOG_DEBUG	        LOG_DEBUG	/* debug-level messages			*/
-#define VFI_DBG_WHEN            0xF             /* Mask for when field                  */
+#define VFI_LOG_WHEN            0xF             /* Mask for when field                  */
 
-#define VFI_DBG_WHO_WHAT        ~VFI_DBG_WHEN   /* Mask for who and what field          */
+#define VFI_DBG_WHO_WHAT        (~VFI_LOG_WHEN) /* Mask for who and what field          */
 
 /* The top-bits of who_what select who, a subsystem, component, type etc. */
-#define VFI_LOG_LOG             0x80000000
-#define VFI_LOG_FRMWRK          0x40000000
-#define VFI_LOG_API             0x20000000
-#define VFI_LOG_WHO             0xfffffe00
+#define VFI_DBG_LOG             0x80000000
+#define VFI_DBG_FRMWRK          0x40000000
+#define VFI_DBG_API             0x20000000
+#define VFI_DBG_WHO             0xfffffe00
 
 /* The bottom bits of who_what select a functional area, what to log */
-#define VFI_LOG_FUNCALL         0x00000010
-#define VFI_LOG_TRACE           0x00000020
-#define VFI_LOG_WHATEVER        0x00000040
-#define VFI_LOG_ERROR           0x00000080
-#define VFI_LOG_WHAT            (~VFI_LOG_WHO & VFI_LOG_WHO_WHAT)
+#define VFI_DBG_FUNCALL         0x00000010
+#define VFI_DBG_TRACE           0x00000020
+#define VFI_DBG_WHATEVER        0x00000040
+#define VFI_DBG_ERROR           0x00000080
+#define VFI_DBG_WHAT            (~VFI_DBG_WHO & VFI_DBG_WHO_WHAT)
 
 /* So from msb to lsb log_level can be thought of as defining who, what, when */
 /* eg Location operators, function calls, when info is wanted */
 /* VFI_DBG_LOCATION | VFI_DBG_FUNCALL | VFI_DBG_INFO */
-#define VFI_LOG_EVERYONE        VFI_LOG_WHO
-#define VFI_LOG_EVERYTHING      VFI_LOG_WHAT
-#define VFI_LOG_ALWAYS          VFI_LOG_WHEN
-#define VFI_LOG_ALL             (VFI_LOG_EVERYONE | VFI_LOG_EVERYTHING | VFI_LOG_ALWAYS)
-#define VFI_LOG_DEFAULT         (VFI_LOG_EVERYONE | VFI_LOG_EVERYTHING | VFI_LOG_ERR)
+#define VFI_DBG_EVERYONE        VFI_DBG_WHO
+#define VFI_DBG_EVERYTHING      VFI_DBG_WHAT
+#define VFI_DBG_ALWAYS          VFI_LOG_WHEN
+#define VFI_DBG_ALL             (VFI_DBG_EVERYONE | VFI_DBG_EVERYTHING | VFI_DBG_ALWAYS)
+#define VFI_DBG_DEFAULT         (VFI_DBG_EVERYONE | VFI_DBG_EVERYTHING | VFI_LOG_ERR)
 
 /* VFI logger */
-//static void vfi_debug(char *format, ...) __attribute__((unused,format(printf,1,2)));
+static void vfi_log(int level, const char *format, ...) __attribute__((unused,format(printf,2,3)));
 static void vfi_log(int level, const char *format, ...)
-{
-	va_list args;
+{	va_list args;
 	va_start(args,format);
-	vsyslog(LOG_USER|(level&VFI_DBG_WHEN), format, args);
+	vsyslog(LOG_USER|(level&VFI_LOG_WHEN), format, args);
 	va_end(args);
 }
 
-#ifdef VFI_DEBUG
-//extern unsigned int vfi_dbg_level;
+#ifdef VFI_DBG
+/* Debug level is treated as a four-bit level integer, when, and a 28-bit mask, who_what */
+static unsigned int vfi_debug_level;
 
-#define VFI_DEBUG(l,f, arg...) if ((((l) & VFI_DBG_WHEN) <= (vfi_debug_level & VFI_DBG_WHEN)) && \
-				     ((((l) & VFI_DBG_WHO_WHAT) & vfi_debug_level ) == ((l) & VFI_DBG_WHO_WHAT)) ) \
-                                     vfi_log((l) & VFI_DBG_WHEN, ## arg)
+#define VFI_DEBUG(l,f, arg...) if ((((l) & VFI_LOG_WHEN) <= (vfi_debug_level & VFI_LOG_WHEN)) && \
+				   ((((l) & VFI_DBG_WHO_WHAT) & vfi_debug_level ) == ((l) & VFI_DBG_WHO_WHAT)) ) \
+		vfi_log((l) & VFI_LOG_WHEN, f, ## arg)
 #define VFI_DEBUG_SAFE(l,c,f,arg...) if ((c)) VFI_DEBUG((l),f, ## arg)
-#define VFI_RESULT(x) ({int _myx = (x); if (_myx) VFI_DEBUG(MY_ERROR,"%s:%d:%s returns error %d\n",__FILE__,__LINE__, __FUNCTION__,_myx) ; _myx;});
+#define VFI_RESULT(x) ({int _myx = (x); if (_myx < 0) VFI_DEBUG(MY_ERROR,"%s:%d:%s returns error %d\n",__FILE__,__LINE__, __FUNCTION__,_myx) ; _myx;});
 
 
 #else
